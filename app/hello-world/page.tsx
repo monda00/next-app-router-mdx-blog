2d199826e427a2a0d594f7bd8abe9eed1c5ffde3
monda00 <monda0524@gmail.com>
2024-02-04 23:00:32 +0900

    Add gray-matter

--- app/hello-world/page.tsx
+++ app/hello-world/page.tsx
@@ -1,14 +1,24 @@
 import { readFile } from 'fs/promises'
+import matter from 'gray-matter'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
 export default async function HelloWorldPost() {
   const markdown = await readFile('contents/posts/hello-world.mdx', {
     encoding: 'utf8',
   })
+  const { content, data } = matter(markdown)
 
   return (
     <div>
-      <MDXRemote source={markdown} />
+      <div>
+        <h1>Frontmatter</h1>
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
+      </div>
+      <div>
+        <h1>Markdown content</h1>
+        <MDXRemote source={content} />
+      </div>
     </div>
   )
 }
